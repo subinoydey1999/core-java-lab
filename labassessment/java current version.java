8db20e2fd0495092387adf40340d72cc534a2742
subinoydey1999 <113093020+subinoydey1999@users.noreply.github.com>
2022-09-08 17:41:10 +0530

    Add files via upload

---
+++ labassessment/java current version.java
@@ -0,0 +1,11 @@
+/*
+program:Java Program Print The Current Version
+@author:subinoy dey
+@date:8 sept 2022
+*/
+//decleration a class
+java Current Version
+public class main{
+public static void main(string[]args){
+system.out.println("java current version");
+}
---
+++ labassessment/quotient and remainder.java
@@ -0,0 +1,16 @@
+/*
+program:Find  Quotient And Remainder
+@author:subinoy dey
+@date:8 sept 2022
+*/
+//decleration a class
+quotient and reminder
+public class main{
+public static void main(string[]args){
+int divident = 49;
+int divisor = 6;
+int quotient = divider / divisor;
+int reminder = divident % divisor;
+system.out.println("quotient = "+ quotient);
+system.outprintln("reminder = " + reminder)
+}
